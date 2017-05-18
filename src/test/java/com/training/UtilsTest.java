842e91bb85c531fc3c7d583e584a86ac0d76bc61
ben abdelali <hassano.ben25@gmail.com>
2017-05-18 18:16:56 +0200

    hassan some test

--- src/test/java/com/training/UtilsTest.java
+++ src/test/java/com/training/UtilsTest.java
@@ -1,5 +1,6 @@
 package com.training;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -10,5 +11,16 @@ public class UtilsTest {
     @Test
     public void test(){
         System.out.println("this is a test");
+    }
+
+    @Test
+    public void hassanTest(){
+        Assert.assertSame("successful", 10 , Utils.add(5,5));
+    }
+
+    @Test
+    public void hassanSecondTest(){
+        Assert.assertSame(15,Utils.add(3,3,3,3,3));
+       // Assert.assertNotSame(10,Utils.add(5,5));
     }
 }
