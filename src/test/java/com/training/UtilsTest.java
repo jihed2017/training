22b68fda981ed66b6afb9f7756ab4d9fd1013b39
Jihed2017 <jojokido66@gmail.com>
2017-05-18 17:54:23 +0200

    test fix

---
+++ src/test/java/com/training/UtilsTest.java
@@ -0,0 +1,14 @@
+package com.training;
+
+import org.junit.Test;
+
+/**
+ * Created by Opleiding on 18/05/2017.
+ */
+public class UtilsTest {
+
+    @Test
+    public void test(){
+        System.out.println("this is a test");
+    }
+}
