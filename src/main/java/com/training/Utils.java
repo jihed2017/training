5a79fb7ab9908df5058601f35a024ef99c073711
Jihed2017 <jojokido66@gmail.com>
2017-05-19 17:40:07 +0200

    tests push

--- src/main/java/com/training/Utils.java
+++ src/main/java/com/training/Utils.java
@@ -10,8 +10,8 @@ public class Utils {
         int result = 0;
         for (int i = 0; i < numbers.length; i++) {
             Integer transform = new Integer(numbers[i]);
-            if(transform==null){
-                transform=0;
+            if (transform == null) {
+                transform = 0;
             }
             result += transform;
         }
@@ -21,11 +21,24 @@ public class Utils {
     public static int add(List<Integer> numbers) {
         int result = 0;
         for (int i = 0; i < numbers.size(); i++) {
-            if(numbers.get(i)!=null){
+            if (numbers.get(i) != null) {
                 result += numbers.get(i);
             }
         }
         return result;
+    }
+
+    public static int mikeMethod(String input) throws MyException {
+        try {
+            if (input != null) {
+                return Integer.parseInt(input);
+            } else {
+                throw new MyException("Please enter a value");
+            }
+        } catch (NumberFormatException e) {
+            throw new MyException("Enter a number");
+        }
+
     }
 }
 
--- src/test/java/com/training/UtilsTest.java
+++ src/test/java/com/training/UtilsTest.java
@@ -11,17 +11,33 @@ import java.util.List;
 /**
  * Created by Opleiding on 18/05/2017.
  */
+import org.junit.*;
+import sun.plugin2.liveconnect.JSExceptions;
+
+import static org.junit.Assert.*;
+
 public class UtilsTest {
 
     @Test
-    public void test() {
+    public void test() throws MyException {
         Integer nb = new Integer(15);
         nb = null;
-        List<Integer> numbersNew = new ArrayList<Integer>();
+        List<Integer> numbersNew = null;
         numbersNew.add(5);
         numbersNew.add(null);
-        int total = Utils.add(nb,4);
-        Assert.assertEquals(5, total);
+        try {
+            int total = Utils.add(numbersNew);
+        } catch (NullPointerException e){
+            throw new MyException("Anything that you want");
+        }
+        System.out.println("continue...");
+        //Assert.assertEquals(5, total);
+    }
+    @Test
+    public void testMikeMethod()throws Exception{
+        String test ="";
+        int result = Utils.mikeMethod(test);
+        assertEquals("Result should be 15",15,result);
     }
 
 
